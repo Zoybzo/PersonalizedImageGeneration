f7e5a9157f517de94e5ea9bf6060dea89b42cdc0
ZHANG HEXIN <1332177205@qq.com>
2024-04-19 16:14:31 +0800

    gradio_demo
    2024年4月19日16:14:19
    add gradio_demo file

--- gradio_demo.py
+++ gradio_demo.py
@@ -1,10 +1,38 @@
 import gradio as gr
 from PIL import Image
+import requests
 
-test_img = Image.open("test_img.jpg")
-def text2image(text):
-    return test_img
+base_url = "http://127.0.0.1:8001/text2image/"
 
-t2i_demo = gr.Interface(fn=text2image, inputs=gr.Textbox(), outputs=gr.Image())
+
+def text2image(image_prompt, type, user_info):
+    if type == "id":
+        url = base_url + "generateByID"
+        params = {
+            "api_key": "123",
+            "user_id": "123",
+            "prompt": image_prompt
+        }
+        response = requests.post(url, data=params)
+    else:
+        url = base_url + "generateByList"
+        params = {
+            "api_key": "123",
+            "id_list": ["123", "123"],
+            "prompt": image_prompt
+        }
+        response = requests.post(url, data=params)
+    return Image.open(response["response"])
+
+
+t2i_demo = gr.Interface(
+    fn=text2image,
+    inputs=[
+        gr.Textbox(placeholder="Enter prompt for image generation", label="Image Prompt"),
+        # gr.Textbox(placeholder="Enter navigator prompt", label="Negative Prompt"),
+        gr.Dropdown(choices=["id", "seq"], label="Choose the type of content"),
+        gr.Textbox(placeholder="Enter user info", label="User Info Input")
+    ],
+    outputs=gr.Image()
+)
 t2i_demo.launch()
-
